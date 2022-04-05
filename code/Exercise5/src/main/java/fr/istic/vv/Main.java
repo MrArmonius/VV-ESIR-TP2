a592dcb8c54cdbbf71bcbb32dd1019fcf5bd63ca
MrArmonius <mrarmonius@gmail.com>
2022-04-05 03:22:16 +0200

    Add code Ex5

---
+++ code/Exercise5/src/main/java/fr/istic/vv/Main.java
@@ -0,0 +1,62 @@
+package fr.istic.vv;
+
+import com.github.javaparser.Problem;
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.visitor.VoidVisitor;
+import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
+import com.github.javaparser.utils.SourceRoot;
+
+import java.io.File;
+import java.io.IOException;  // Import the IOException class to handle errors
+import java.io.FileWriter;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        if(args.length == 0) {
+            System.err.println("Should provide the path to the source code");
+            System.exit(1);
+        }
+
+        File file = new File(args[0]);
+        if(!file.exists() || !file.isDirectory() || !file.canRead()) {
+            System.err.println("Provide a path to an existing readable directory");
+            System.exit(2);
+        }
+
+        
+        try {
+            File myObj = new File("Report.txt");
+            if (myObj.createNewFile()) {
+              System.out.println("File created: " + myObj.getName());
+            } else {
+              System.out.println("File already exists.");
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+
+        try {
+            FileWriter myWriter = new FileWriter("Report.txt");
+            myWriter.write("");
+            myWriter.close();
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+
+        SourceRoot root = new SourceRoot(file.toPath());
+        PublicElementsPrinter printer = new PublicElementsPrinter();
+        root.parse("", (localPath, absolutePath, result) -> {
+            result.ifSuccessful(unit -> unit.accept(printer, null));
+            return SourceRoot.Callback.Result.DONT_SAVE;
+        });
+    }
+
+
+}
---
+++ code/Exercise5/src/main/java/fr/istic/vv/PublicElementsPrinter.java
@@ -0,0 +1,154 @@
+package fr.istic.vv;
+
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.body.*;
+import com.github.javaparser.ast.visitor.VoidVisitorWithDefaults;
+import com.github.javaparser.ast.expr.*;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.HashSet;
+
+import java.io.IOException;  // Import the IOException class to handle errors
+import java.io.FileWriter;
+import java.io.BufferedWriter;
+
+// This class visits a compilation unit and
+// prints all public enum, classes or interfaces along with their public methods
+public class PublicElementsPrinter extends VoidVisitorWithDefaults<Void> {
+
+    private  Map<String, VariableDeclarator> getterBook = new HashMap<String, VariableDeclarator>();
+    private  Map<String, Set<String>> methodVariables = new HashMap<String, Set<String>>();
+    private  Map<String, Map<String, Set<String>>> PairNodeClass = new HashMap<String, Map<String, Set<String>>>();
+
+    private int numberMethod = 0;
+
+    @Override
+    public void visit(CompilationUnit unit, Void arg) {
+        for(TypeDeclaration<?> type : unit.getTypes()) {
+            type.accept(this, null);
+        }
+    }
+
+    public void visitTypeDeclaration(TypeDeclaration<?> declaration, Void arg) {
+        System.out.print(declaration.getFullyQualifiedName().orElse("[Anonymous]") + " or simply " + declaration.getName() + "\n");
+        for(FieldDeclaration field : declaration.getFields()) {
+            field.accept(this, arg);
+        }
+
+        for(MethodDeclaration method : declaration.getMethods()) {
+            method.accept(this, arg);
+        }
+
+        
+        // Printing nested types in the top level
+        for(BodyDeclaration<?> member : declaration.getMembers()) {
+            if (member instanceof TypeDeclaration)
+                member.accept(this, arg);
+        }
+        
+        try {
+            FileWriter myWriter = new FileWriter("Report.txt", true);
+            BufferedWriter bufWrite = new BufferedWriter(myWriter);
+            bufWrite.write(declaration.getFullyQualifiedName().orElse("[Anonymous]") + " or simply " + declaration.getName() + "\n");
+            Set<String> getterKeys = methodVariables.keySet();
+            for(String key : getterKeys) {
+                bufWrite.write("   " + key + " - "+ methodVariables.get(key) + "\n");
+            }
+            bufWrite.write("\n" + "TCC score: "+ calculateTCC() + "\n");
+            bufWrite.close();
+            System.out.println("Successfully wrote to the file.");
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+
+        numberMethod = 0;
+        methodVariables.clear();
+        getterBook.clear();
+    }
+
+    @Override
+    public void visit(ClassOrInterfaceDeclaration declaration, Void arg) {
+        visitTypeDeclaration(declaration, arg);
+    }
+
+    @Override
+    public void visit(FieldDeclaration declaration, Void arg) {
+        //if(declaration.isPublic()) return;
+        for(VariableDeclarator var: declaration.getVariables()) {
+            String nomSimple = var.getName().toString();
+            getterBook.put(nomSimple, var);
+        }
+        
+    }
+
+    @Override
+    public void visit(MethodDeclaration declaration, Void arg) {
+        //if(!declaration.isPublic()) return;
+        numberMethod++;
+        String nameMethod = declaration.getDeclarationAsString();
+        System.out.println("  " + nameMethod);
+        //methodVariables.put(nameMethod, new HashSet<String>());
+        declaration.findAll(NameExpr.class).stream()
+            .filter(name -> getterBook.containsKey(name.getName().toString()))
+            .forEach(name -> forEachMethod(name, nameMethod));
+
+        
+    }
+
+    private void forEachMethod(NameExpr name, String nameMethod) {
+        String nameVariable = name.getName().toString();
+
+        System.out.println("    "+nameVariable);
+        if(!methodVariables.containsKey(nameVariable)){
+            methodVariables.put(nameVariable, new HashSet<String>());
+        }
+        methodVariables.get(nameVariable).add(nameMethod);
+    }
+
+    private Map<String, Set<String>> calculatePairNodes() {
+        Map<String, Set<String>> pairNodes = new HashMap<String, Set<String>>();
+
+        
+
+        
+
+        // Create pairNodes
+        Set<String> methodKeys = methodVariables.keySet();
+        for(String key : methodKeys) {
+
+            Set<String> variables = methodVariables.get(key);
+            for(String variable : variables) {
+
+                for(String variable2 : variables) {
+                    if(variable.compareTo(variable2) != 0) {
+                        String namePair = variable + " -> " + variable2;
+                        if(!pairNodes.containsKey(namePair)) {
+                            pairNodes.put( namePair, new HashSet<String>());
+                        }
+                        pairNodes.get(namePair).add(key);
+                    }
+                }
+                
+            }
+            
+        }
+
+        return pairNodes;
+    }
+
+    private String calculateTotalPairNodes() {
+        // Calculate the total numbers of pairs which is possible
+        int NumberOfMethods = numberMethod;
+        int AllPairsOfNodes = (NumberOfMethods * (NumberOfMethods-1)) / 2;
+        String tailleMax = "/"+ AllPairsOfNodes;
+        return tailleMax;
+    }
+
+    private String calculateTCC() {
+        return calculatePairNodes().keySet().size() + calculateTotalPairNodes();
+    }
+
+}
